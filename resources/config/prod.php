1d7fd8132cb8d0c442d44759d23d21379f39b36a
Raphael Khaiat <raphael@khaiat.org>
2013-03-03 22:17:03 +0100

    Wenizit v0.1

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -26,6 +26,8 @@ $app['assetic.output.path_to_css']      = 'css/styles.css';
 $app['assetic.input.path_to_js']        = array(
     $app['assetic.input.path_to_assets'] . 'js/bootstrap.min.js',
     $app['assetic.input.path_to_assets'] . 'js/script.js',
+    $app['assetic.input.path_to_assets'] . 'js/countdown.js',
+    $app['assetic.input.path_to_assets'] . 'js/plugins.js',
 );
 $app['assetic.output.path_to_js']       = 'js/scripts.js';
 $app['assetic.filter.yui_compressor.path'] = '/usr/share/yui-compressor/yui-compressor.jar';
--- src/controllers.php
+++ src/controllers.php
@@ -10,6 +10,58 @@ $app->match('/', function() use ($app) {
     return $app['twig']->render('index.html.twig');
 })->bind('homepage');
 
+$app->match('/countdown/{id}', function($id) use ($app) {
+    if (!is_numeric($id)) {
+        $app->redirect($app['url_generator']->generate('homepage'));
+    }
+
+    // Get the value in db.
+    $sql = 'SELECT id,title,date,password FROM countdowns where id = ?';
+    $countdown = $app['db']->fetchAssoc($sql, array((int) $id));
+    if (!is_array($countdown)) {
+        return $app->redirect($app['url_generator']->generate('homepage'));
+    }
+
+    $form = null;
+    $auth = true;
+    // If this page is protected.
+    if ($countdown['password']) {
+        $auth = false;
+        $builder = $app['form.factory']->createBuilder('form');
+        $form = $builder
+            ->add('password', 'password')
+            ->getForm();
+
+        if ('POST' === $app['request']->getMethod()) {
+            $form->bind($app['request']);
+
+            if ($form->isValid()) {
+                $sent_pass = $form->get('password')->getData();
+                if (sha1($sent_pass) == $countdown['password']) {
+                    $auth = true;
+                    $app['session']->getFlashBag()->add(
+                        'success', 'Right password :)'
+                    );
+                }
+                else {
+                    $app['session']->getFlashBag()->add(
+                        'error', 'Wrong password'
+                    );
+                }
+            }
+        }
+    }
+
+    $date = new Datetime($countdown['date']);
+
+    return $app['twig']->render('countdown.html.twig', array(
+        'title' => $countdown['title'],
+        'date'  => $date->format('d F Y H:i:s'),
+        'auth'  => $auth,
+        'form'  => $form ? $form->createView() : null
+    ));
+})->bind('cd-see');
+
 $app->match('/add', function() use ($app) {
     $builder = $app['form.factory']->createBuilder('form');
 
@@ -19,9 +71,16 @@ $app->match('/add', function() use ($app) {
             'constraints' => new Assert\NotBlank(),
             'attr'        => array('placeholder' => 'Title')
         ))
-        ->add('date', 'datetime', array('data' => $default_date->format('Y-m-d H:i:s'), 'input' => 'string'))
+        ->add('date', 'datetime', array(
+          'data' => $default_date->format('Y-m-d H:i:s'),
+          'input' => 'string'
+        ))
         ->add('password', 'password', array(
           'attr' => array('placeholder' => 'Leave empty for public link')
+        ))
+        ->add('antispam', 'text', array(
+          'constraints' => new Assert\NotBlank(),
+          'attr'        => array('placeholder' => 'What`s the app name ?')
         ))
         ->getForm();
 
@@ -29,30 +88,39 @@ $app->match('/add', function() use ($app) {
         $form->bind($app['request']);
 
         if ($form->isValid()) {
-            $title = $form->get('title')->getData();
-            $date = $form->get('date')->getData();
-            $pass = $form->get('password')->getData();
+            $spam = $form->get('antispam')->getData();
+            if ($spam == "wenizit") {
+                $title = $form->get('title')->getData();
+                $date = $form->get('date')->getData();
+                $pass = $form->get('password')->getData();
 
-            if ($title && $date) {
-                echo $title.'<br>';
-                var_dump($date).'<br>';
-                echo $pass.'<br>';
-                if (new Datetime($date) < new Datetime()) {
-                    $app['session']->getFlashBag()
-                      ->add('error', 'You can\'t set a past date');
+                if ($title && $date) {
+                    if (new Datetime($date) < new Datetime()) {
+                        $app['session']->getFlashBag()
+                          ->add('error', 'You can\'t set a past date');
+                    }
+                    else {
+                        $cd = array(
+                            'title' => $title,
+                            'date' => $date,
+                        );
+                        if ($pass) {
+                            $cd['password'] = sha1($pass);
+                        }
+
+                        $app['db']->insert('countdowns', $cd);
+                        $id = $app['db']->lastInsertId();
+                        $app['session']->getFlashBag()
+                            ->add('success', 'Your countdown ' . $title . ' has been saved');
+                        return $app->redirect($app['url_generator']->generate(
+                            'cd-see', array('id' => $id)
+                        ));
+                    }
                 }
                 else {
-                    $cd = array(
-                        'title' => $title,
-                        'date' => $date,
-                        'password' => $pass,
-                    );
-                    $app['db']->insert('countdowns', $cd);
+                    $app['session']->getFlashBag()
+                        ->add('error', 'Title and date are mandatory');
                 }
-            }
-            else {
-                $app['session']->getFlashBag()
-                    ->add('error', 'Title and date are mandatory');
             }
         }
     }
